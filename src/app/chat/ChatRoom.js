6136c617ab6ec817f251d8d812ea02ef80ec50c6
Alexis Laronga <alexislaronga@Alexiss-MacBook-Air.local>
2023-08-14 18:01:53 -0400

    feat: created a chat system

--- src/app/chat/ChatRoom.js
+++ src/app/chat/ChatRoom.js
@@ -1,36 +1,13 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
+import React from 'react';
 
-const ChatRoom = ({ roomId }) => {
-    const [messages, setMessages] = useState([]);
-
-    useEffect(() => {
-        // Fetch messages for the specified chat room
-        axios.get(`${process.env.NEXT_PUBLIC_SERVER_URL}/chats/${roomId}/messages`)
-            .then(response => {
-                setMessages(response.data.messages);
-            })
-            .catch(error => {
-                console.error('Error fetching messages:', error);
-            });
-    }, [roomId]);
+export default function MessageContent({ content }) {
+    console.log('content:', content);
 
     return (
-        <div>
-            <h1>Chat Room</h1>
-            <ul>
-                {messages.map(message => (
-                    <li key={message._id}>
-                        <div>
-                            <strong>{message.sender.firstName}: </strong>
-                            {message.content}
-                        </div>
-                        <div>{message.createdAt}</div>
-                    </li>
-                ))}
-            </ul>
+        <div className="message">
+            <div className="message-header">
+                <p>{content}</p>
+            </div>
         </div>
     );
-};
-
-export default ChatRoom;
+}
